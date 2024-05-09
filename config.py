d34790267e5e9d023040dd4125ba70b1692e4a67
Fancy <129271532+Halffancyy@users.noreply.github.com>
2024-05-09 20:44:17 +0800

    Update config.py
    Change the database file from being saved in the root directory to being saved in the instance folder.

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import os
 
-# Return the directory of the current executable file
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config(object):
     SECRET_KEY = os.urandom(24)  # Used for encrypting sessions
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
